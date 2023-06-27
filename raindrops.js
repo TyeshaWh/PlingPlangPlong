f8294d29aa5f3ed76a94bb833db5f719c5749503
Admin <admin.push@hackerrank.workspace>
2023-06-27 20:48:39 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- raindrops.js
+++ raindrops.js
@@ -3,6 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export const convert = () => {
+/* export const convert = () => {
   throw new Error('Remove this statement and implement this function');
-};
+}; */
+
+// 3 = pling
+// 5 = plang
+// 7 = plong
+
+
+//string that contains raindrop sounds
+
+let  arrSound = ['Pling' , 'Plang' , 'Plong']
+
+console.log(arrSound.length)
